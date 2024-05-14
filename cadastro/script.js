f4171e03c899a25a65490f6558b7225d78ac6976
VictorSantosSilvaa <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-05-14 16:27:19 -0300

    tentando

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,27 +1,61 @@
-const formulario = document.getElementById('meuFormulario');
+/* const formulario = document.getElementById('meuFormulario');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
 
 formulario.addEventListener('botao', (e) => {
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+
 
     e.preventDefault();
 
     //transformar os dados em JSON
-    const userDate = JSON.stringify({
+    const userData = JSON.stringify({
         name: name.value,
         email: email.value,
         password: password.value
     })
 
-    fetch("http://10.92.198.38:8080/auth/signin", {
-        method: "Post",
-        body: userDate,
+    fetch("http://10.92.198.38:8080/auth/signup", {
+        method: "POST",
+        body: userData,
         headers: { 'Content-Type': 'application/json ; charset=UTF-8' }
     }).then(result => result.json())
         .then((data) => console.log(data))
         .catch((err) => console.log(err));
-        
-})
+    console.log(userData)
+}) */
 
-console.log('aplicacao foi')
+const button = document.getElementById("botao");
+const nameu = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+button.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    const userData = JSON.stringify({
+        name: nameu.value,
+        email: email.value,
+        password: password.value,
+
+    });
+    console.log(userData)
+
+    fetch("http://10.92.198.38:8080/auth/signup", {
+        method: "POST",
+        body: userData,
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data)
+            window.location.href = "/login/index.html";
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
--- postagem/script.js
+++ postagem/script.js
@@ -1,21 +1,16 @@
-const formulario = document.getElementById('meuFormulario');
+/* const formulario = document.getElementById('meuFormulario');
 
 formulario.addEventListener('botao', (e) => {
-    const name = document.getElementById('email');
-    const email = document.getElementById('password');
-    const password = document.getElementById('botao');
 
     e.preventDefault();
 
     //transformar os dados em JSON
     const userDate = JSON.stringify({
-        name: email.value,
-        email: password.value,
-        password: botao.value
+
     })
 
 
-    fetch("http://localhost:8080/auth/signup", {
+    fetch("http://localhost:8080/feed/post", {
         method: "Post",
         body: userDate,
         headers: { 'Content-Type': 'application/json ; charset=UTF-8' }
@@ -23,4 +18,33 @@ formulario.addEventListener('botao', (e) => {
         .then((data) => console.log(data))
         .catch((err) => console.log(err));
 
+}) */
+
+
+const form = document.getElementById("form");
+ 
+form.addEventListener("submit", function (e) {
+ 
+    const title = document.getElementById("titulo");
+    const content = document.getElementById("descricao");
+    const image = document.getElementById("img");
+    const token = localStorage.getItem('token');
+ 
+    e.preventDefault();
+ 
+    const formData = new FormData();
+    formData.append("image", image.files[0]);
+    formData.append("title", title.value);
+    formData.append("content", content.value);
+ 
+    fetch("http://10.92.198.38:8080/feed/post", {
+        method: "POST",
+        body: formData,
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+        .then((result) => result.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
 })
